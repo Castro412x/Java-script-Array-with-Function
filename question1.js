4c591a6de27d411f53f9e4db930e72da0b6c8105
Nonyerem Emmanuel <emmanuelnonyerem2@gmail.com>
2026-03-26 09:52:28 +0100

    Add month index task

---
+++ question1.js
@@ -0,0 +1,7 @@
+function monthIndex(){
+    let months = ["January", "February", "March", "April", "May"]
+    console.log(`Second month is ${months[1]}`);
+    console.log(`Fourth month is ${months[3]}`);   
+}
+
+monthIndex()
