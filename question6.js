54348a4408152bfa86a50c9501a819ce78fc480a
Nonyerem Emmanuel <emmanuelnonyerem2@gmail.com>
2026-03-26 12:11:25 +0100

    Add goals checker task

---
+++ question6.js
@@ -0,0 +1,10 @@
+function goalsChecker(){
+    let goals = [2,0,3,1,4,0,2];
+    let count = 0;
+    for(i = 0;i < goals.length;i++)
+        if(goals[i] === 0){
+            count++
+        }
+        console.log(count)
+}
+goalsChecker()
