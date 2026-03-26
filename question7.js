78d739b2c3e9ea892dea30f08630a4c8edf5329f
Nonyerem Emmanuel <emmanuelnonyerem2@gmail.com>
2026-03-26 12:17:33 +0100

    Add students list task

---
+++ question7.js
@@ -0,0 +1,8 @@
+function classListChecker(){
+    let classlist = ["Ifeco","UD","Winner","Jenny","Smart","Steve"]
+    for(i = 0;i < classlist.length; i++){
+        console.log(`Student name ${i + 1}: ${classlist[i]}`)
+}
+}
+classListChecker()
+        
