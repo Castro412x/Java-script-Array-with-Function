a1541a4fd31a7379afd3ca34ea721f3c8fa06515
Nonyerem Emmanuel <emmanuelnonyerem2@gmail.com>
2026-03-27 07:54:18 +0100

    Add result checker

---
+++ question13.js
@@ -0,0 +1,10 @@
+function resultChecker(){
+    let results = [55,40,72,38,91,65,48];
+    for(i = 0;i<results.length;i++){
+        if(results[i] > 50){
+            console.log(`${results[i]} - Pass`)
+        }
+        else console.log(`${results[i]} - Fail`)
+    }
+}
+resultChecker()
